fab203110566d3be6ad92097d16040f39e87bf2a
quasigod <quasigod-io@protonmail.com>
2021-10-30 04:19:48 -0400

    Cleaned up i3 config a little and changed polybar launch script.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -11,10 +11,11 @@ bool=1
 
 while [ $bool -eq 1 ]
 do
-    running=$(pgrep -l pipewire | awk '/pipewire$/ {printf $2}')
-    if [[ $running == 'pipewire' ]]
+    running=$(pgrep -l easyeffects | awk '/easyeffects$/ {printf $2}')
+    if [[ $running == 'easyeffects' ]]
     then
         bool=0
+        echo shid
     fi
 done
 
