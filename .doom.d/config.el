7dbc8806651e44e1b0d10610ba99bb3ee4c24eaa
quasigod <quasigod-io@protonmail.com>
2021-11-02 01:38:13 -0400

    Added tabnine to doom and icon to update moduyle in polybar

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,13 +1,8 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Michael Belsanti"
-      user-mail-address "quasigod-io@protonmail.com")
+;; sync' after modifying this file!  Some functionality uses this to identify you, e.g. GPG configuration, email clients, file templates and snippets.  (setq user-full-name "Michael Belsanti"
+(setq user-mail-address "quasigod-io@protonmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -15,17 +10,17 @@
 ;; + `doom-font'
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;;   presentations or streaming. They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 (setq doom-font(font-spec :family "Cascadia Code" :size 16 :weight 'regular))
 
-;; This section makes Cascadia's ligatures work, remove/comment if you switch fonts.
-;; (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+;; This section breaks shit wouldn't recommend uncommenting
+;;-------------------------------------------------------------------------------------------------------------------------------------------
+;;
+;; This section makes Cascadia's ligatures work, remove/comment if you switch fonts. (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
 ;;                (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
 ;;                (36 . ".\\(?:>\\)")
 ;;                (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
@@ -54,6 +49,7 @@
 ;;  (dolist (char-regexp alist)
 ;;    (set-char-table-range composition-function-table (car char-regexp)
 ;;                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
+;;-------------------------------------------------------------------------------------------------------------------------------------------
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -87,7 +83,7 @@
 ;; they are implemented.
 
 (setq
- projectile-project-search-path '("~/Projects/"))
+ projectile-project-search-path '("~/projects/"))
 
 (global-set-key (kbd "C-h") 'evil-window-left)
 (global-set-key (kbd "C-j") 'evil-window-down)
@@ -97,3 +93,10 @@
 (global-set-key (kbd "C-S-J") '+evil/window-move-down)
 (global-set-key (kbd "C-S-K") '+evil/window-move-up)
 (global-set-key (kbd "C-S-L") '+evil/window-move-right)
+
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+(setq company-idle-delay 0)
+(setq lsp-completion-mode :enabled)
+(setq company-idle-delay 0)
+(setq company-show-quick-access t)
