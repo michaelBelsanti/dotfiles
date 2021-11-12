c1c15ea27e743a1006a1516e0eecbbe7f1a59ffd
quasigod <quasigod-io@protonmail.com>
2021-11-12 02:13:25 -0500

    Removed a lot of transparency and turned off rounded corners for a more minimal look.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
-killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
+# Delay to prevent alsa module from breaking
+#sleep 2
 
-# Launch bar1 and bar2
-#
+# Waits for easyeffects to prevent alsa module from breaking
 bool=1
 
 while [ $bool -eq 1 ]
@@ -15,12 +12,17 @@ do
     if [[ $running == 'easyeffects' ]]
     then
         bool=0
-        echo shid
     fi
 done
 
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
 polybar secondary 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
+
+pamixer --source $(pamixer --list-sources | awk '/Blue/ {print $1}') --set-volume 40
