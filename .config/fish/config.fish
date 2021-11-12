e04cb386ab764c3113342969c73c6b96aebf26e0
quasigod <quasigod-io@protonmail.com>
2021-11-12 02:14:10 -0500

    Added fish back as it was not meant to be removed -- yet

---
+++ .config/fish/config.fish
@@ -0,0 +1,59 @@
+﻿## Set values
+# Hide welcome message
+set fish_greeting
+set VIRTUAL_ENV_DISABLE_PROMPT "1"
+export EDITOR=nvim
+export VISUAL=nvim
+
+# exa > ls
+alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
+alias l.="exa -a | egrep '^\.'"                                     # show only dotfiles
+
+# bat > cat
+alias cat='bat --style header --style rules --style snip --style changes --style header'
+
+# Common use
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias dir='dir --color=auto'
+alias broot='broot -h'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias hw='hwinfo --short'                                   # Hardware Info
+alias big="expac -H M '%m\t%n' | sort -h | nl"              # Sort installed packages according to size in MB (expac must be installed)
+alias code='vscodium'
+alias vim='nvim'
+alias please='sudo'
+alias cleanup='paru -Rns (paru -Qtdq)'
+alias update='paru -Syu'
+alias upgrade='paru -Syu'
+alias rmcache='paru -Scc'
+alias yay='paru'
+alias nnn='nnn -H'
+alias aura='sudo aura'
+alias wing++='i686-w64-mingw32-g++ -static-libgcc -static-libstdc++ -static'
+alias LinuxGames='cd /run/media/quasigod/LinuxGames'
+alias gitup='git add . && git commit -m "Update" && git push'
+alias untar='tar xvf'
+
+set -x TERM alacritty
+
+## Run neofetch if session is interactive
+if status --is-interactive
+#   fet.sh 
+   pokemon-colorscripts -r
+end
+
+set thefuck fuck -hard 
+
+thefuck --alias | source
+
+export ZDOTDIR="$HOME/.config/zsh"
