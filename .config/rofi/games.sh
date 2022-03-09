bcbbe5d0be9ef9de433a1dc9c63f9855d5f71100
quasigod <quasigod-io@protonmail.com>
2022-03-08 23:28:29 -0500

    .

--- .config/rofi/games.sh
+++ .config/rofi/games.sh
@@ -4,14 +4,16 @@ cd $(dirname $(realpath $0))
 
 GAME_LAUNCHER_CACHE=$HOME/.cache/rofi-game-launcher
 
-./update-game-entries.sh -q &
+./scripts/update-game-entries.sh -q &
 
-# Temporarily overwrite XDG_DATA_HOME so that Rofi looks for
-# .desktop files in $GAME_LAUNCHER/applications instead of
-# ~/.local/share/applications
-export XDG_DATA_HOME=$GAME_LAUNCHER_CACHE
-rofi -show drun -theme games -drun-categories SteamLibrary \
-     -cache-dir $GAME_LAUNCHER_CACHE
+(
+    # Temporarily overwrite XDG_DATA_HOME so that Rofi looks for
+    # .desktop files in $GAME_LAUNCHER/applications instead of
+    # ~/.local/share/applications
+    export XDG_DATA_HOME=$GAME_LAUNCHER_CACHE
+    rofi -show drun -theme games -drun-categories SteamLibrary \
+         -cache-dir $GAME_LAUNCHER_CACHE
+)
 
 # Emulate most recently used history by resetting the count
 # to 0 for each application
