64b0172e46e17557491b8bd6eb71faf17c53ca08
quasigod <quasigod-io@protonmail.com>
2022-06-18 03:27:57 -0400

    Finally fixed exa error when pressing enter in zsh. Also fixed Neovide font.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
+vim.o.guifont = "JetBrainsMono Nerd Font:8"
+
 require("colorscheme")
 require("keymaps")
-require("neovide")
+-- require("neovide")
 require("options")
 require("plugin")
 require("plugins")
--- .config/nvim/lua/neovide.lua
+++
@@ -1,3 +0,0 @@
-vim.o.guifont = "JetBrainsMono Nerd Font:12"
-
--- vim.g["guifont"] = "JetBrainsMono Nerd Font:12"
